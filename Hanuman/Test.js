03068d95d5fde76099e3dcea2c799848ed1ece7e
Bunty835 <108917541+Bunty835@users.noreply.github.com>
2023-12-26 13:16:35 +0530

    Add files via upload

---
+++ Hanuman/Test.js
@@ -0,0 +1,33 @@
+// Question 1. Diffrence b/w Js Variables
+// Ans:- Let = its blockscope varibale which is run within function or scope.
+//       var = its not a blockscope varibale which is run Global scope.
+//       Const = its blockscope varibale which is run within function or scope its value is always constant and cant changed after declariation.
+
+
+// Question 2.Diff b/w == & === 
+//Ans:- == is equal to  which is not checked a data type then comparing two variables .
+//      Ex:- . 3 == 3 > True   4=='4' > True
+
+//     === is strict equal which is firstly checked data type then comparing two variables.
+//     Ex:-  3 == 3 > True   4=='4' > False
+
+// Question 3. Create arrow function.
+// const add = (a, b) => {
+//     return a + b;
+//   };
+// Ex:- let arr = [12,18,45,23,98,76,56,90,84,68] 
+// const newArray = arr.filter(num => num > 50);
+// console.log(newArray);
+
+
+  // Question 4.  print a new array all numbers are greater then 50
+//   let arr = [12,18,45,23,98,76,56,90,84,68] 
+// const newArray = arr.filter(num => num > 50);
+// console.log(newArray);
+
+
+  // Question 5. write a code to hide a button.
+  // Question 6.  write a code to hide and show a password.
+
+
+
